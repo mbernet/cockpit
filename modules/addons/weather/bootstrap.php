c3b31799aca88237f2e6934f7e5177f6369b1fb4
Marc Bernet <mbernet@gmail.com>
2016-03-14 12:38:35 +0100

    improvements

--- modules/addons/weather/bootstrap.php
+++ modules/addons/weather/bootstrap.php
@@ -3,6 +3,12 @@
 define('OPENWEATHER_API_KEY', '91313229aedf333d6446e32571a7aab0');
 define('OPENWEATHER_IBIZA_ID', '2516479');
 
+/*
+ * Considerar ús de CACHE
+*/
+
+include_once(__DIR__.'/apc.caching.php');
+
 $app->bind("/weather", function($params) use ($routes) {
     $token = $this->param("token", false);
     //echo $token;
@@ -17,20 +23,28 @@ $app->bind("/weather", function($params) use ($routes) {
         return ["error" => "access denied"];
     }
 
-    $query = array(
-        'id'    => OPENWEATHER_IBIZA_ID,
-        'appid' => OPENWEATHER_API_KEY
-    );
+    $oCache = new CacheAPC();
 
-    $qString = http_build_query($query);
-    $url = 'http://api.openweathermap.org/data/2.5/weather?'.$qString;
+    if($result = $oCache->getData('ibiza_weather')) {
+        return $result;
+    } else {
+        $query = array(
+            'id'    => OPENWEATHER_IBIZA_ID,
+            'appid' => OPENWEATHER_API_KEY
+        );
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
+        $qString = http_build_query($query);
+        $url = 'http://api.openweathermap.org/data/2.5/weather?'.$qString;
 
-    curl_close($ch);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
 
-    return $output;
+        curl_close($ch);
+        $oCache->setData('ibiza_weather', $output);
+        return $output;
+    }
+
+
 });
